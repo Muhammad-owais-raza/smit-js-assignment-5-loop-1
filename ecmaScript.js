b62cd2740e88dab4cfe3d07ff8af0ca6c9280e56
Muhammad-owais-raza <owaisraza.codes@gmail.com>
2026-07-07 02:40:58 +0500

    This assignment was completed

---
+++ ecmaScript.js
@@ -0,0 +1,11 @@
+// is ma user jo bhi number enter kry ga us ka table print ho ga
+
+let table = Number(prompt("Enter your number"));
+
+// let n = 1;
+
+for (let i = 1; i <= 12; i++) {
+  console.log(table + " * " + i, " = ", i * table);
+}                        /* n++ ya bhi sahi ha lakin Programming practice ke hisab sa 
+                            n ki zarurat nahi ha. lakin 
+                            Assignment k hisaab sa galat nahi ha. */
